a82df4a865ced41cc741217af11b0b3f66ace809
Maros Sofranko <maros@farma-hradisko.sk>
2022-11-14 11:13:15 +0100

    AddSearchUsersFunction

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,30 +9,42 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
-        loading: true
+        loading: false
     }
 
     const [state, dispatch] = useReducer(githubReducer, initialState);
 
-    const fetchUsers = async () => {
-        const response = await fetch(`${GITHUB_URL}/users`, {
+    const searchUsers = async (text) => {
+        setLoading();
+        const response = await fetch(`${GITHUB_URL}/search/users?q=${text}`, {
             heareds: {
                 Authorization: `token ${GITHUB_TOKEN}`
             }
         });
 
-        const data = await response.json();
+        const {items} = await response.json();
         
         dispatch({
             type: "GET_USERS",
-            payload: data
+            payload: items
+        })
+    }
+
+    const setLoading = () => {
+        dispatch({type: "SET_LOADING"});
+    }
+
+    const clearUsers = () => {
+        dispatch({
+            type: "CLEAR_USERS",
         })
     }
 
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers
+        searchUsers,
+        clearUsers
     }}>
         {children}
     </GithubContext.Provider>
