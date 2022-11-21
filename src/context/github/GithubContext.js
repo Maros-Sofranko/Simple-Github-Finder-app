b3ade71af55edb5cfda64b3a3e97be1cf38afae4
Maros Sofranko <maros@farma-hradisko.sk>
2022-11-21 10:23:22 +0100

    AddGithubContext

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -6,9 +6,10 @@ const GithubContext = createContext();
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-export const GithubProvider = ({children}) => {
+export const GithubProvider = ({ children }) => {
     const initialState = {
         users: [],
+        user: {},
         loading: false
     }
 
@@ -22,16 +23,36 @@ export const GithubProvider = ({children}) => {
             }
         });
 
-        const {items} = await response.json();
-        
+        const { items } = await response.json();
+
         dispatch({
             type: "GET_USERS",
             payload: items
         })
     }
 
+    const getUser = async (login) => {
+        setLoading();
+        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+            heareds: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        });
+
+        if (response.status === 404) {
+            window.location = "/notfound";
+        } else {
+            const data = await response.json();
+
+            dispatch({
+                type: "GET_USER",
+                payload: data
+            })
+        }
+    }
+
     const setLoading = () => {
-        dispatch({type: "SET_LOADING"});
+        dispatch({ type: "SET_LOADING" });
     }
 
     const clearUsers = () => {
@@ -43,8 +64,10 @@ export const GithubProvider = ({children}) => {
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
         searchUsers,
-        clearUsers
+        clearUsers,
+        getUser
     }}>
         {children}
     </GithubContext.Provider>
